6938be3cff6e9415877ebb04272240dca09f62db
NicolaBoniello <114167768+NicolaBoniello@users.noreply.github.com>
2023-11-30 16:10:45 +0100

    Add files via upload

---
+++ test-fixing/src/main/java/com/nicol/testfixing/TestFixingApplication.java
@@ -0,0 +1,13 @@
+package com.nicol.testfixing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFixingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFixingApplication.class, args);
+	}
+
+}
---
+++ test-fixing/src/test/java/com/nicol/testfixing/TestFixingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicol.testfixing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFixingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
